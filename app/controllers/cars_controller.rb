3ee7b41464e1416dfd37d0cf9bcfd72cde9e3c3b
Salomon Beza <beza.salomon@gmail.com>
2017-02-16 15:51:28 +0200

    created new, destroy, create actions

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,11 +1,26 @@
 class CarsController < ApplicationController
-  def index
-    @cars = Unirest.get("http://localhost:3000/api/v1/cars.json").body
-  end
 
   def show
     @car = Unirest.get("http://localhost:3000/api/v1/cars/#{params[:id]}.json").body
     render :show
   end
 
-end
+   def index
+    @cars = Unirest.get("http://localhost:3000/api/v1/cars.json").body
+  end
+
+  def new
+
+  end
+
+  def create
+     @car = Unirest.post("http://localhost:3000/api/v1/cars.json", :headers => {"Accept"=> "application/json"}, :parameters => {:name => params[:name], :model => params[:model], :manufacturer=> params[:manufacturer]}).body
+     redirect_to "/cars/#{@car['id']}"
+  end
+
+  def destroy
+    @car = Unirest.delete("http://localhost:3000/api/v1/cars/#{params[:id]}.json").body
+    redirect_to "/cars"
+  end
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
- get"/cars", to:'cars#index'
- get"cars/:id", to:'cars#show'
+  get"/cars", to:'cars#index'
+  get "/cars/new", to:'cars#new'
+  get"/cars/:id", to:'cars#show'
+  post "/cars", to:'cars#create'
+  delete "/cars/:id", to:'cars#destroy'
 end
